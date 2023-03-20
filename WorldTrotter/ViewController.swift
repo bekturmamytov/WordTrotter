164bf1faa44da123d8efa27f13d3dbc251c0d81e
Bektur Mamytov <bekturmamytov@gmail.com>
2023-03-20 21:10:50 +0900

    commit before deleting the constraints  as in the book

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        
+        gradient.colors = [UIColor.systemBlue.cgColor,
+                           UIColor.systemCyan.cgColor,
+                           UIColor.systemMint.cgColor,
+                           UIColor.systemMint.cgColor]
+        
+        gradient.shouldRasterize = true
+        view.layer.insertSublayer(gradient, at: 0)
+    }
     
 }
 
