46307b7095ce3efa86995ef159ef84aee4d2fa71
Bektur Mamytov <bekturmamytov@gmail.com>
2023-03-22 07:17:15 +0900

    commit: finished one more chapter this commit has been applied before the bronze challenge

---
+++ WorldTrotter/View Controllers/ConversionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Bektur Mamytov on 18/3/23.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("ConversionViewController loaded again")
+    }
+}
+
---
+++ WorldTrotter/View Controllers/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Bektur Mamytov on 21/3/23.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("MapViewController loaded again")
+    }
+
+}
