39d3ecf68c96a4bdc0508688be942da653d09949
Bektur Mamytov <bekturmamytov@gmail.com>
2023-03-22 21:48:17 +0900

    commit before the challenge on main branch

--- WorldTrotter/View Controllers/MapViewController.swift
+++ WorldTrotter/View Controllers/MapViewController.swift
@@ -6,9 +6,59 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        //Add segmented control
+        
+        let segmentedControl = UISegmentedControl(items: ["Standart", "Hybrid", "Sattelite"])
+        segmentedControl.backgroundColor = .systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
+        
+        // Creating constraints
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        //Activating constraints
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+            case 0:
+                mapView.mapType = .standard
+            case 1:
+                mapView.mapType = .hybrid
+            case 2:
+                mapView.mapType = .satellite
+            default:
+                break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded")
